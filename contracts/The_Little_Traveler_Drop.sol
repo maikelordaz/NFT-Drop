2947d0f898e4fb13dc13e7ed93484fd883ba69ef
maikelordaz <maikelordaz@gmail.com>
2022-02-21 21:51:19 -0400

    fourht push

--- contracts/The_Little_Traveler_Drop.sol
+++ contracts/The_Little_Traveler_Drop.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: unlicensed
 /**
 * @title TheTraveler NFT drop
 * @author Maikel Ordaz
@@ -17,6 +17,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 //LIBRARIES IMPORTS
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
 //CONTRACT 
 
@@ -33,14 +34,26 @@ contract TheLittleTraveler is
     bytes32 public constant minter = keccak256("minter");
     bytes32 public constant whitelisted = keccak256("whitelisted");
     Counters.Counter private _nftId;
+    uint whitelistMintFee = 0.005 ether;
     uint mintFee = 0.01 ether;
+    enum Status {whitelist, minters}
+    Status public mintingStatus;
    
+//MODIFIERS
 
-//MAPPINGS
+    modifier actualStatus(Status _mintingStatus) {
+        
+        require(mintingStatus == _mintingStatus);
+        _;
 
+    }
 
 //EVENTS
 
+    event mintPaused(string);
+    event mintUnPaused(string);
+    event normalMint (string);
+    event mintFinished (string);
 
 //CONSTRUCTOR
 
@@ -64,6 +77,8 @@ contract TheLittleTraveler is
         onlyRole(DEFAULT_ADMIN_ROLE) {
             
             _pause();
+            emit mintPaused("The mint is paused.");
+            console.log("The mint is still paused");
 
     }
 
@@ -72,14 +87,13 @@ contract TheLittleTraveler is
         onlyRole(DEFAULT_ADMIN_ROLE) {
             
             _unpause();
+            mintingStatus = Status.whitelist;
+            emit mintUnPaused("The whitelist has started.");            
+            console.log("If you are from the whitelist, you can mint now!");
 
     }
 
-    /**
-    * @dev this functions are related to he minting process.
-      Only the minters can call this functions.
-      The contract has to be unpaused. 
-     */
+    /// @dev this functions allow the addresses from the whitelist to mint.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
@@ -89,7 +103,35 @@ contract TheLittleTraveler is
         super._beforeTokenTransfer(from, to, tokenId);
 
     }
+    
+    function whitelistmint(address to) 
+        public 
+        whenNotPaused
+        onlyRole(whitelisted)
+        actualStatus(Status.whitelist)
+        payable {
 
+        require(msg.value == whitelistMintFee);
+        uint256 tokenId = _nftId.current();
+        for(uint i = 0; i < 4; i++) {
+
+            _nftId.increment();
+            _safeMint(to, tokenId);
+
+        }
+
+        mintingStatus = Status.minters;
+        emit normalMint("The mint is paused.");            
+        console.log("The whitelist has finished, from now on, everyone can mint!");
+
+    }
+
+    /**
+    * @dev this functions are related to he minting process.
+      Only the minters can call this functions.
+      The contract has to be unpaused. 
+    */
+  
     function mint(address to) 
         public 
         whenNotPaused
@@ -98,23 +140,31 @@ contract TheLittleTraveler is
 
         require(msg.value == mintFee);
         uint256 tokenId = _nftId.current();
-        for(uint i = 0; i < 10; i++) {
+        for(uint j = 4; j < 10; j++) {
 
             _nftId.increment();
             _safeMint(to, tokenId);
 
         }
 
+        emit mintFinished("The mint has finished.");            
+        console.log("Every token has been minted");
+
     }
+
+    /// @dev This function is to set the URI of the Token.
 
     function tokenURI(uint256 tokenId)
         public
         view
         override (ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+        returns (string memory) {
+
             return super.tokenURI(tokenId);
+
     }
+
+    /// @dev This function is to burn the NFT delete the Token´s Id and the Token´s URI.
 
     function _burn(uint256 tokenId) 
         internal 
@@ -124,5 +174,13 @@ contract TheLittleTraveler is
 
     }
 
-   
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    
 }

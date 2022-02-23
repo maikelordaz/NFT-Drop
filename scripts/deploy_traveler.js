6cba1e6850de4acf3724a1c0c8a41702677b15b4
maikelordaz <maikelordaz@gmail.com>
2022-02-23 00:22:54 -0400

    seventh

--- scripts/deploy_traveler.js
+++ scripts/deploy_traveler.js
@@ -2,14 +2,14 @@ const hre = require("hardhat");
 
 async function main() {
   
-  // await hre.run('compile');
+  await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
 
   
   const TheLittleTraveler = await hre.ethers.getContractFactory("TheLittleTraveler");
-  const thelittletraveler_contract = await TheLittleTraveler.deploy("The little traveler NFT!");
-
+  const thelittletraveler_contract = await TheLittleTraveler.deploy();
   await thelittletraveler_contract.deployed();
-
   console.log("TheLittleTraveler deployed to:", thelittletraveler_contract.address);
 }
 
